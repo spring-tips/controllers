e49b650c6de578fd3f6b912cea8043c6a5089491
Josh Long <josh@joshlong.com>
2021-10-04 13:10:57 -0700

    rest

--- rest/src/main/java/com/example/rest/RestApplication.java
+++ rest/src/main/java/com/example/rest/RestApplication.java
@@ -3,6 +3,7 @@ package com.example.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.NestedExceptionUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class RestApplication {
@@ -33,9 +38,17 @@ class GreetingRestController {
         return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/greetings/{name}")
+    @GetMapping("/greeting/{name}")
     Mono<Greeting> greeting(@PathVariable String name) {
         Assert.isTrue(Character.isUpperCase(name.charAt(0)), () -> "the name must start with a capital letter");
         return Mono.just(new Greeting("Hello, " + name + "!"));
+    }
+
+    @GetMapping(value = "/greetings/{name}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Greeting> greetings(@PathVariable String name) {
+        return Flux
+                .fromStream(Stream.generate(() -> new Greeting("Hello, " + name + "!")))
+                .take(100)
+                .delayElements(Duration.ofSeconds(1));
     }
 }
